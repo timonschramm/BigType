ccfac5685720be45a66315482cd1df6c00ade04e
timonschramm <timonschramm@t-online.de>
2024-05-06 15:37:27 +0200

    Initial Commit

---
+++ Big Type/Big_TypeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Big_TypeApp.swift
+//  Big Type
+//
+//  Created by Timon schramm on 06.05.24.
+//
+
+import SwiftUI
+
+@main
+struct Big_TypeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
